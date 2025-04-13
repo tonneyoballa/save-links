38651883d08b32e991f262b17f34442447bdfb39
Tonney Oballa <tonneyoballah@gmail.com>
2025-04-13 14:48:30 -0700

    save

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ document.addEventListener("DOMContentLoaded", function () {
       linkElement.target = "_blank";
 
       const deleteBtn = document.createElement("button");
-      deleteBtn.textContent = "🗑️";
+      deleteBtn.textContent = "img";
       deleteBtn.style.marginLeft = "10px";
       deleteBtn.onclick = () => deleteLink(link);
 
